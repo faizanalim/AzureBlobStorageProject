c0671a4512f5b233e747effeb3563adafd2be837
faizanalim <faizan.23@hotmail.com>
2025-11-28 01:16:12 +0500

    Add project files.

---
+++ AzureBlobProject/Services/ContainerService.cs
@@ -0,0 +1,26 @@
+﻿
+namespace AzureBlobProject.Services
+{
+    public class ContainerService : IContainerService
+    {
+        Task IContainerService.CreateContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IContainerService.DeleteContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<string>> IContainerService.GetAllContainer()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<string>> IContainerService.GetAllContainerAndBlobs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
