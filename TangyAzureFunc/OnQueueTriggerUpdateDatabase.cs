c18b15960ba933b3734641752cc7e56cd21a1dcd
faizanalim <faizan.23@hotmail.com>
2025-11-29 21:18:50 +0500

    Save To Local Database

--- TangyAzureFunc/OnQueueTriggerUpdateDatabase.cs
+++ TangyAzureFunc/OnQueueTriggerUpdateDatabase.cs
@@ -1,15 +1,15 @@
+using System;
 using Azure.Storage.Queues.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using TangyAzureFunc.Data;
-
+using TangyAzureFunc.Models;
 
 namespace TangyAzureFunc
 {
-
     public class OnQueueTriggerUpdateDatabase
     {
-
         private readonly ILogger<OnQueueTriggerUpdateDatabase> _logger;
         private readonly ApplicationDbContext _dbContext;
 
@@ -22,9 +22,19 @@ namespace TangyAzureFunc
         [Function(nameof(OnQueueTriggerUpdateDatabase))]
         public void Run([QueueTrigger("SalesRequestInBound")] QueueMessage message)
         {
-            _logger.LogInformation($"C# Queue trigger function processed: {message.MessageText}");
+            string messageBody = message.Body.ToString();
+            SalesRequest? salesRequest = JsonConvert.DeserializeObject<SalesRequest>(messageBody);
+
+            if (salesRequest != null)
+            {
+                salesRequest.Status = "";
+                _dbContext.SalesRequests.Add(salesRequest);
+                _dbContext.SaveChanges();
+            }
+            else
+            {
+                _logger.LogWarning("Failed to deserialize the message body into a SalesRequest object.");
+            }
         }
-
     }
-
 }
